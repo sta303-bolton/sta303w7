4b63ef1ccf050f10b8fa630c2bffab798262cfe6
Liza Bolton <lizabolton@gmail.com>
2021-03-02 17:37:02 -0500

    Trying to make import better

---
+++ R/ignoreme.R
@@ -0,0 +1,11 @@
+#' You can ignoer this
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#' @import SMPracticals dplyr magrittr
+#' @examples
+youcanignorethis <- function(x){
+  mean(x, na.rm = TRUE)
+}
